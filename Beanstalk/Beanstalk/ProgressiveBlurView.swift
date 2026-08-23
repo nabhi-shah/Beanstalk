dd4aacd908fedc363c97a507dbb62f343b9e8a1e
Nabhi <nabhishah@gmail.com>
2026-08-22 20:19:13 -0400

    Implement Design+Code VariableBlurView

--- Beanstalk/Beanstalk/ProgressiveBlurView.swift
+++ Beanstalk/Beanstalk/ProgressiveBlurView.swift
@@ -1,4 +1,69 @@
 import SwiftUI
+import UIKit
+import CoreImage.CIFilterBuiltins
+
+public enum VariableBlurDirection {
+    case blurredTopClearBottom
+    case blurredBottomClearTop
+}
+
+public struct VariableBlurView: UIViewRepresentable {
+    public var maxBlurRadius: CGFloat = 20
+    public var direction: VariableBlurDirection = .blurredTopClearBottom
+    public var startOffset: CGFloat = 0
+
+    public func makeUIView(context: Context) -> VariableBlurUIView {
+        VariableBlurUIView(maxBlurRadius: maxBlurRadius, direction: direction, startOffset: startOffset)
+    }
+
+    public func updateUIView(_ uiView: VariableBlurUIView, context: Context) {}
+}
+
+open class VariableBlurUIView: UIVisualEffectView {
+    public init(maxBlurRadius: CGFloat = 20, direction: VariableBlurDirection = .blurredTopClearBottom, startOffset: CGFloat = 0) {
+        super.init(effect: UIBlurEffect(style: .regular))
+
+        guard let CAFilter = NSClassFromString("CAFilter") as? NSObject.Type else { return }
+        guard let variableBlur = CAFilter.self.perform(NSSelectorFromString("filterWithType:"), with: "variableBlur")?.takeUnretainedValue() as? NSObject else { return }
+
+        let gradientImage = makeGradientImage(startOffset: startOffset, direction: direction)
+
+        variableBlur.setValue(maxBlurRadius, forKey: "inputRadius")
+        variableBlur.setValue(gradientImage, forKey: "inputMaskImage")
+        variableBlur.setValue(true, forKey: "inputNormalizeEdges")
+
+        let backdropLayer = subviews.first?.layer
+        backdropLayer?.filters = [variableBlur]
+
+        for subview in subviews.dropFirst() {
+            subview.alpha = 0
+        }
+    }
+
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    open override func didMoveToWindow() {
+        guard let window, let backdropLayer = subviews.first?.layer else { return }
+        backdropLayer.setValue(window.screen.scale, forKey: "scale")
+    }
+
+    open override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {}
+
+    private func makeGradientImage(width: CGFloat = 100, height: CGFloat = 100, startOffset: CGFloat, direction: VariableBlurDirection) -> CGImage {
+        let ciGradientFilter = CIFilter.linearGradient()
+        ciGradientFilter.color0 = CIColor.black
+        ciGradientFilter.color1 = CIColor.clear
+        ciGradientFilter.point0 = CGPoint(x: 0, y: height)
+        ciGradientFilter.point1 = CGPoint(x: 0, y: startOffset * height)
+        if case .blurredBottomClearTop = direction {
+            ciGradientFilter.point0.y = 0
+            ciGradientFilter.point1.y = height - ciGradientFilter.point1.y
+        }
+        return CIContext().createCGImage(ciGradientFilter.outputImage!, from: CGRect(x: 0, y: 0, width: width, height: height))!
+    }
+}
 
 enum BlurEdge {
     case top, bottom
@@ -9,19 +74,7 @@ struct ProgressiveBlurView: View {
     var edge: BlurEdge = .top
     
     var body: some View {
-        Rectangle()
-            .fill(.ultraThinMaterial)
-            .mask(
-                LinearGradient(
-                    gradient: Gradient(stops: [
-                        .init(color: .black, location: 0.0),
-                        .init(color: .black, location: 0.15),
-                        .init(color: .clear, location: 1.0)
-                    ]),
-                    startPoint: edge == .top ? .top : .bottom,
-                    endPoint: edge == .top ? .bottom : .top
-                )
-            )
+        VariableBlurView(maxBlurRadius: 20, direction: edge == .top ? .blurredTopClearBottom : .blurredBottomClearTop)
             .frame(height: height)
             .ignoresSafeArea()
     }

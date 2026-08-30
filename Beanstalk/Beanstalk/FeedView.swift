e7e8a24aba468a4279774484a54e69aa9d4c138d
Nabhi <nabhishah@gmail.com>
2026-08-30 11:45:44 -0400

    UI: Adjust gap between article cards to 26

--- Beanstalk/Beanstalk/FeedView.swift
+++ Beanstalk/Beanstalk/FeedView.swift
@@ -82,7 +82,7 @@ struct FeedContentView: View {
                 }
                 .frame(height: 0)
                 
-                LazyVStack(spacing: 24) {
+                LazyVStack(spacing: 26) {
                     ForEach(MockData.articles) { article in
                         NavigationLink(destination: ArticleDetailView(article: article)) {
                             ArticleRowView(article: article)

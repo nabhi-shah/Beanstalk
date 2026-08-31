78cc516e5c2f4dcf1cb5a61917c55ebaa52d915e
Nabhi <nabhishah@gmail.com>
2026-08-30 20:53:32 -0400

    Restore touch location tracking for ripple effect using transparent hit test view

--- Beanstalk/Beanstalk/RippleModifier.swift
+++ Beanstalk/Beanstalk/RippleModifier.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct RippleModifier: ViewModifier {
     var rippleColor: Color
+    var touchLocation: CGPoint?
     var isPressed: Bool
     
     @State private var progress: CGFloat = 1.0
@@ -9,9 +10,10 @@ struct RippleModifier: ViewModifier {
     func body(content: Content) -> some View {
         content
             .visualEffect { viewContent, geometryProxy in
-                viewContent.colorEffect(
+                let center = touchLocation ?? CGPoint(x: geometryProxy.size.width / 2, y: geometryProxy.size.height / 2)
+                return viewContent.colorEffect(
                     ShaderLibrary.default.lightRipple(
-                        .float2(CGPoint(x: geometryProxy.size.width / 2, y: geometryProxy.size.height / 2)),
+                        .float2(center),
                         .float(progress),
                         .float2(geometryProxy.size),
                         .color(rippleColor)
@@ -32,14 +34,45 @@ struct RippleModifier: ViewModifier {
 struct RippleButtonStyle: ButtonStyle {
     var rippleColor: Color
     
+    @State private var touchLocation: CGPoint?
+    
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
-            .modifier(RippleModifier(rippleColor: rippleColor, isPressed: configuration.isPressed))
+            .modifier(RippleModifier(rippleColor: rippleColor, touchLocation: touchLocation, isPressed: configuration.isPressed))
+            .background(
+                TouchLocatingView { location in
+                    if !configuration.isPressed {
+                        touchLocation = location
+                    }
+                }
+            )
+    }
+}
+
+struct TouchLocatingView: UIViewRepresentable {
+    var onUpdate: (CGPoint) -> Void
+
+    func makeUIView(context: Context) -> UIView {
+        let view = TouchLocatingUIView()
+        view.onUpdate = onUpdate
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
+
+    class TouchLocatingUIView: UIView {
+        var onUpdate: ((CGPoint) -> Void)?
+        
+        override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+            // hitTest is called multiple times, capture the location
+            onUpdate?(point)
+            return nil // Do not intercept touches!
+        }
     }
 }
 
 extension View {
     func rippleEffect(color: Color = .white, isPressed: Bool = false) -> some View {
-        self.modifier(RippleModifier(rippleColor: color, isPressed: isPressed))
+        self.modifier(RippleModifier(rippleColor: color, touchLocation: nil, isPressed: isPressed))
     }
 }

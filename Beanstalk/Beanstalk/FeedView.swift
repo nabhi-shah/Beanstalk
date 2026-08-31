965fcffacf8b604e61fdb39fe7f6ca51d911301c
Nabhi <nabhishah@gmail.com>
2026-08-30 21:16:42 -0400

    Adjust top padding to 48 for article stack

--- Beanstalk/Beanstalk/FeedView.swift
+++ Beanstalk/Beanstalk/FeedView.swift
@@ -117,7 +117,7 @@ struct FeedContentView: View {
                     }
                 }
                 .padding(.horizontal, 12)
-                .padding(.top, 24)
+                .padding(.top, 48)
                 .padding(.bottom, 120) // Space for TabBar
             }
             .coordinateSpace(name: "scroll")
